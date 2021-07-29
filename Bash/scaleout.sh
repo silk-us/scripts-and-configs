c5f1526517ec435b20ae62916f65884a5edd2d72
JayAreP <43276115+JayAreP@users.noreply.github.com>
2021-07-29 08:06:51 -0700

    Create scaleout.sh

---
+++ Bash/scaleout.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Run this script like so:
+# scaleout.sh 10.10.1.20 10.10.1.22 8
+# This will query an existing iscsisession on 10.10.1.20, add a session on IP 10.10.1.22, 
+# and re-balance the sessions to 8 per interface
+
+
+sdpdatainterface=$1
+sdpnode3interface=$2
+sessionsper=$3
+
+target=`sudo iscsiadm -m node session | grep $sdpdatainterface | awk  '{ print $2 }'`
+
+sudo iscsiadm -m discovery -t sendtargets -p $sdpdatainterface
+sudo iscsiadm -m node -T $target -p $sdpnode3interface= -o update -n node.session.nr_sessions -v $sessionsper
+
+sudo iscsiadm -m node --login

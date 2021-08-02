ae2952c73434359e58b4f21b6a63515100dd12f1
steve-chi <61326785+steve-chi@users.noreply.github.com>
2021-08-02 14:39:15 -0400

    Update scaleout_session_rebalance.sh
    Removed unnecessary iscsiadm commands prior to for-loop.
    Adjusted for-loop condition to include last index.

--- Bash/scaleout_session_rebalance.sh
+++ Bash/scaleout_session_rebalance.sh
@@ -10,16 +10,13 @@ sdpnewnodeinterface=$2
 sessionsper=$3
 
 target=`sudo iscsiadm -m node session | grep $sdpdatainterface | awk  '{ print $2 }'`
-
-sudo iscsiadm -m discovery -t sendtargets -p $sdpdatainterface
-sudo iscsiadm -m node -T $target -p $sdpnewnodeinterface -o update -n node.session.nr_sessions -v $sessionsper
-sudo iscsiadm -m node --login
-
 startIndex=`echo $sdpdatainterface | awk -F. '{print $NF}'`
 endIndex=`echo $sdpnewnodeinterface | awk -F. '{print $NF}'`
 ipPrefix=`echo $sdpdatainterface | rev | cut -d "." -f2- | rev`
 
-for ((i = $startIndex ; i < $endIndex ; i++))
+sudo iscsiadm -m discovery -t sendtargets -p $sdpdatainterface
+
+for ((i = $startIndex ; i <= $endIndex ; i++))
 do
         ipTarget="${ipPrefix}.${i}"
         sudo iscsiadm -m node -T $target -p $ipTarget --logout

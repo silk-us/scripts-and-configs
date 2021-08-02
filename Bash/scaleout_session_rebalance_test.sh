6e4cc2a0ae44da42256c1d7df1b7ea74cfe25d90
JayAreP <43276115+JayAreP@users.noreply.github.com>
2021-08-02 14:19:27 -0700

    Update scaleout_session_rebalance_test.sh

--- Bash/scaleout_session_rebalance_test.sh
+++ Bash/scaleout_session_rebalance_test.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # Run this script with a single argument of the first SDP data interface IP like so:
 # scaleout_session_rebalance.sh 10.10.1.20 
-# This will query an existing iscsisession on 10.10.1.20
 
+# This sets the primary cnode interface sequence
 sdpdatainterface=$1
 
+# This gathers the current iscsi target information and sets the variables required for the iscsiadm commands.
 target=`sudo iscsiadm -m node session | grep $sdpdatainterface | awk  '{ print $2 }'`
 startIndex=`echo $sdpdatainterface | awk -F. '{print $NF}'`
 endIndex=`expr $startIndex + 8`
 ipPrefix=`echo $sdpdatainterface | rev | cut -d "." -f2- | rev`
 
+# This loop simply tests which cnode interfaces are serving iscsi using the above variables. 
 check=0
 for ((i = $startIndex ; i <= $endIndex ; i++))
 do
@@ -22,9 +24,11 @@ do
                 fi
 done
 
+# This calculates the number of sessions per path that will be set. 
 sessionsper=`expr 24 / $check`
 echo $sessionsper
 
+# This loop performs the actual iscsiadm commands to retire old sessions, create new sessins, and set the appropriate number of paths per session.
 for ((i = $startIndex ; i <= $endIndex ; i++))
 do
         ipTarget="${ipPrefix}.${i}"

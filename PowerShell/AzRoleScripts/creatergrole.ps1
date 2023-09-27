3d3ed233998518d96427c86e8f54a3032abe4416
J.R. Phillips <43276115+JayAreP@users.noreply.github.com>
2023-09-27 14:15:21 -0700

    Update creatergrole.ps1

--- PowerShell/AzRoleScripts/creatergrole.ps1
+++ PowerShell/AzRoleScripts/creatergrole.ps1
@@ -61,8 +61,6 @@ $actions = @(
     "Microsoft.Storage/storageAccounts/listServiceSas/action"
     "Microsoft.Storage/storageAccounts/listAccountSas/action"
     "Microsoft.Storage/storageAccounts/write"
-    "Microsoft.Storage/storageAccounts/blobServices/containers/blobs/write" 
-    "Microsoft.Storage/storageAccounts/blobServices/containers/blobs/delete" 
 )
 
 $rolescope.Name = $name

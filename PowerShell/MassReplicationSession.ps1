f02ce40eaca90e7f5c048e88079b599f12a1cfb3
J.R. Phillips <43276115+JayAreP@users.noreply.github.com>
2023-11-13 12:15:31 -0700

    script updates

---
+++ PowerShell/MassReplicationSession.ps1
@@ -0,0 +1,28 @@
+param(
+    [parameter(Mandatory)]
+    $filename,
+    [parameter(Mandatory)]
+    $peername
+)
+
+<#
+    .SYNOPSIS
+    Example for generating numerous replication sessions fed via text file list of desired volume groups
+
+    .EXAMPLE 
+    ./MassReplicationSession.ps1 -filename inputlist.txt -peername replication-peer 
+
+    .NOTES
+    Authored by J.R. Phillips (GitHub: JayAreP)
+
+#>
+
+$vglist = Get-Content $filename
+foreach ($v in $vglist) {
+    $repsessionname = $v + "-rep" 
+    if ($repSessionName.Length -gt 41) {
+        $repSessionName = $vg.id + "-" + (get-random)
+    }
+    New-SDPReplicationSession -name $repSessionName -volumeGroupName $v -replicationPeerName $peername -retentionPolicyName Replication_Retention -externalRetentionPolicyName Replication_Retention -RPO 1200 -mapped | Start-SDPReplicationSession 
+
+}

7052ad86dacea44b6f93a4b041f291a8b7fe884a
J.R. Phillips <jr@pipefl.com>
2025-07-08 13:38:25 -0700

    Update diskspd_4c.ps1

--- PowerShell/diskspd_4c.ps1
+++ PowerShell/diskspd_4c.ps1
@@ -7,6 +7,8 @@ Expand-Archive .\DiskSpd.ZIP -force
 
 $iopsDAT = $drive + 'Testdata\iops.dat'
 $throughputDAT = $drive + 'Testdata\throughput.dat'
+$localName = $env:computername
+
 
 Write-Verbose "-- Creating IO files - $iopsDAT and $throughputDAT --" -Verbose
 New-Item -Name "testdata" -Path $drive -ItemType Directory -ErrorAction SilentlyContinue
@@ -14,20 +16,24 @@ New-Item -Name "testdata" -Path $drive -ItemType Directory -ErrorAction Silently
 .\DiskSpd\amd64\diskspd.exe -d60 -W15 -C15 -c64G -t2 -o4 -b128k -L -r -Sh -w50 $throughputDAT
 
 Write-Verbose "-- Using - $iopsDAT --" -Verbose
-Write-Verbose "-- Running Read IOPS --" -Verbose
 # Read IOPS
-.\DiskSpd\amd64\diskspd.exe -b4K -W15 -d60 -Sh -L -t192 -r -w0 -f32G E:\Testdata\iops.dat > read_iops.txt
+$outfile = $localName + '-' + 'read_iops.txt'
+Write-Verbose "-- Running Read IOPS -- outfile $outfile -- " -Verbose
+.\DiskSpd\amd64\diskspd.exe -b4K -W15 -d60 -Sh -L -t192 -r -w0 -f32G E:\Testdata\iops.dat > $outfile
 
-Write-Verbose "-- Running Write IOPS --" -Verbose
 # Write IOPS
-.\DiskSpd\amd64\diskspd.exe -b4K -W15 -d60 -Sh -L -t192 -r -w100 -f32G E:\Testdata\iops.dat > write_iops.txt
+$outfile = $localName + '-' + 'write_iops.txt'
+Write-Verbose "-- Running Write IOPS -- outfile $outfile -- " -Verbose
+.\DiskSpd\amd64\diskspd.exe -b4K -W15 -d60 -Sh -L -t192 -r -w100 -f32G E:\Testdata\iops.dat > $outfile
 
-Write-Verbose "-- Using - $iopsDAT --" -Verbose
-Write-Verbose "-- Running Read throughput --" -Verbose
+Write-Verbose "-- Using - $throughputDAT --" -Verbose
 # Read throughput
-.\DiskSpd\amd64\diskspd.exe -b128k -W15 -d60 -Sh -L -t32 -r -w0 -f64G E:\Testdata\throughput.dat > read_tp.txt
+$outfile = $localName + '-' + 'read_tp.txt'
+Write-Verbose "-- Running Read throughput -- outfile $outfile -- " -Verbose
+.\DiskSpd\amd64\diskspd.exe -b128k -W15 -d60 -Sh -L -t32 -r -w0 -f64G E:\Testdata\throughput.dat > $outfile
 
-Write-Verbose "-- Running Write throughput --" -Verbose
 # Write throughput
-.\DiskSpd\amd64\diskspd.exe -b128k -W15 -d60 -Sh -L -t32 -r -w100 -f64G E:\Testdata\throughput.dat > write_tp.txt
+$outfile = $localName + '-' + 'write_tp.txt'
+Write-Verbose "-- Running Write throughput -- outfile $outfile -- " -Verbose
+.\DiskSpd\amd64\diskspd.exe -b128k -W15 -d60 -Sh -L -t32 -r -w100 -f64G E:\Testdata\throughput.dat > $outfile
 
